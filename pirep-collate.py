256c853a03d9cf1da711a0336f6b953fa8161ff4
Harriet Parsons <h.parsons@eaobservatory.org>
2017-11-11 11:09:16 -1000

    working on pire-collate.py
    I have it to the point where I can locate all the files produced within
    a time span. I can go in and grab the feedback and convert the feedback
    into meaningful numerical values.

--- pirep-collate.py
+++ pirep-collate.py
@@ -3,6 +3,12 @@
 This script will go in and grab information from 
 a pi report form collate all the information and 
 produce stats/plots for board reports/those interested
+
+Note I do not do this by region it only matters that any issues
+are addressed and that we address areas that need it. 
+
+inputs: -y year -s semester
+i.e. -y 2017 -s A
 '''
 
 
@@ -58,15 +64,124 @@ unixstarttime = time.mktime(startdate.timetuple())
 
 unixendtime = time.mktime(enddate.timetuple())
 
-print (startyear, startmonth, 1 ,int(unixstarttime))
+print (startyear, startmonth, 1, int(unixstarttime))
 
-print (endyear, endmonth, 31 ,int(unixendtime))
+print (endyear, endmonth, 31, int(unixendtime))
 
 # ------------------------------------------- #
 #      grabbing relavent report files         #
 # ------------------------------------------- #
 
+pth = '/web/data/reports/JCMT/'
 
-filelocation = '/web/data/reports/JCMT/'
+import os	
+from glob import glob
+
+filenames = [os.path.basename(x) for x in glob(pth+"pirep_*.txt")]
+
+#print(type(filenames))
+
+myfiles=[]
+
+
+for x in filenames:
+	timestamp = x[6:16] # only correct for pirep	
+#	print (x,timestamp)
+	if unixstarttime <= int(timestamp) <= unixendtime:
+		print ("pirep_{0}.txt".format(timestamp))
+		myfiles.append("{0}pirep_{1}.txt".format(pth,timestamp))
+print (myfiles)
+
+	
+# -------------------------------------------- #
+#      grabbing info from report files         #
+# -------------------------------------------- #	
+
+
+
+def ratingvalue(stringprovided,wheretosearch):
+	'''
+	the function is used to convert a qualitive rating 
+	into a quantative value.
+	'''
+	ratings = {'not applicable': None, 'very dissatisfied': 0, 'dissatisfied': 1, 'satisfied': 2, 'very satisfied': 3}
+	if stringprovided in wheretosearch:
+		print (stringprovided)
+		print (wheretosearch)
+		if ":   very satisfied" in wheretosearch:
+			ratevalue = ratings['very satisfied']
+		if  ":   satisfied" in wheretosearch:
+			ratevalue = ratings['satisfied']
+		if  ":   dissatisfied" in wheretosearch:
+			ratevalue = ratings['dissatisfied']
+		if ":   very dissatisfied" in wheretosearch:
+			ratevalue = ratings['very dissatisfied']
+		if  ":   not applicable" in wheretosearch:
+			ratevalue = ratings['not applicable']
+		print (ratevalue)
+#		return ratevalue
+
+
+	
+
+# maybe need to define a function to turn a line into a rating.
+
+for x in myfiles:
+	with open(x) as input_data:
+		print (x)
+		for line in input_data:
+			if "Percentage Completed:" in line:
+				percom = (line[-7:-2]).strip()
+				print ("completion = {0}%".format(percom))
+
+#			if "Please rate proposal system ease of" in line:
+#				rateline = line				
+#			if "OT ease of use.:" in line:
+#			if "OT documentation.:" in line: 
+#			if "OT support.:" in line:  
+#			if "OMP interface to download your data.:" in line: 
+#			if "CADC interface for searching the JCMT Science Archive and downloading data.:" in line:
+#			if "Starlink software installation.:" in line:
+#			if "Starlink software packages for analysis, reduction, and visualisation.:" in line:
+#			if "heterodyne data reduction documentation.:" in line: 
+#			if "SCUBA-2 data reduction documentation.:" in line: 
+#			if "data reduction pipelines (ORAC-DR).:" in line: 
+#			if "science-products of the data reduction pipelines.:" in line:  
+
+
+ 			ratedataquality = ratingvalue("quality of data obtained.:",line)
+ 	#		print(ratedataquality)
+
+
+# 			if "quality of data obtained.:" in line:
+# 				rateline = line
+# 				print (rateline)
+# 				if ":   very satisfied" in rateline:
+# 					print ('very satisfied')
+# 					ratedataquality = ratings['very satisfied']
+# 				if  ":   satisfied" in rateline:
+# 					ratedataquality = ratings['satisfied']
+# 				if  ":   dissatisfied" in rateline:
+# 					ratedataquality = ratings['dissatisfied']
+# 				if ":   very dissatisfied" in rateline:
+# 					ratedataquality = ratings['very dissatisfied']
+# 				if  ":   not applicable" in rateline:
+# 					ratedataquality = ratings['not applicable']
+# 				print (ratedataquality)
+				
+#			if "satisfied were you with the level of communication with your Friend of Project.:" in line:
+			rateomp = ratingvalue("ease and usefulness of OMP project home page.:",line)
+			rateweb = ratingvalue("satisfied were you with the web site.:",line)
+
+#Completion of project in terms of hours:   0
+# Please rate usefulness of data collected/level of completeness.: 
+
+# Please rate likelihood of data leading to publication.: 	
+
+# -------------------------------------------- #
+#      plotting info from report files         #
+# -------------------------------------------- #
+
+# in essence I want one plot to show median value (as a bar) and SD (as an error bar) for each rating.
 
 
